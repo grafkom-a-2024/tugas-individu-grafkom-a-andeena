70b1f0d2474006b6d103fbfa938a91e73837e27e
andeena <andinabks@gmail.com>
2024-09-22 23:41:19 +0700

    tugas-tiga

---
+++ tugas-tiga/cone/app.js
@@ -0,0 +1,22 @@
+function setup() {
+    createCanvas(400, 400, WEBGL);
+  }
+  
+  function draw() {
+    background(0)
+    
+    let locX = mouseX - height;
+    let locY = mouseY - width;
+    
+    pointLight(0, 0, 255, locX/2, locY/2, 250);
+  
+    
+    normalMaterial();
+    translate(-10, 50, 0);
+    push();
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.03);
+    cone(80, 130);
+    pop();
+  }
+  
